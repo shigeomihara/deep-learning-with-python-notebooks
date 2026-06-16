e29039e0455cbdef5ea88b9f3f4addf020f5a430
Shigeomi Hara <harashigeomi@gmail.com>
2026-06-16 22:59:50 +0900

    gigabytePC WSL

--- MyChap13/LSTM.py
+++ MyChap13/LSTM.py
@@ -1,7 +1,16 @@
+import keras
+from keras import layers
+
 class LSTM:
     def __init__(self, dataFile):
         self.dataFile = dataFile
         self.sequence_length = 120
 
         print("self.dataFile.num_train_samples=", self.dataFile.num_train_samples)
+        print("self.dataFile.raw_data.shape=", self.dataFile.raw_data.shape)
+
+        inputs = keras.Input(shape=(self.sequence_length, self.dataFile.raw_data.shape[-1]))
+        lstm = layers.LSTM(16)
+        x = lstm(inputs)
+        outputs = layers.Dense(1)(x)
         
